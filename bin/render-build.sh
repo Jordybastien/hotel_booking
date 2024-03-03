ee39aa718fa9f8bf80b6fb8a3944766a722080cf
RUGUMBIRA Jordy Bastien <42216593+Jordybastien@users.noreply.github.com>
2024-03-03 20:17:47 +0200

    add platform x86_64-linux for render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,3 +5,7 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+# No worries it's just a small app and free version of render is not allowing me to migrate data
+bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rails db:seed
